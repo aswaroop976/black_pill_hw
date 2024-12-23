3edf01eec5f35174b191c770d00c2adcc07e0780
Arpan Swaroop <arpan.swaroop@gmail.com>
2024-12-23 13:15:15 -0600

    blinking works, black magic fr fr

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,31 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
+use panic_halt as _; // Panic handler
 use stm32f4::stm32f411;
 
 #[entry]
 fn main() -> ! {
-    // Take ownership of the device peripherals
+    // Get access to the device peripherals
     let dp = stm32f411::Peripherals::take().unwrap();
 
-    // Enable GPIOA clock in the RCC (Reset and Clock Control) register
+    // Enable GPIOC clock in RCC
     let rcc = dp.RCC;
-    rcc.ahb1enr.modify(|_, w| w.gpioaen().set_bit());
+    rcc.ahb1enr.modify(|_, w| w.gpiocen().set_bit());
 
-    // Configure PA5 as a push-pull output
-    let gpioa = dp.GPIOA;
-    gpioa.moder.modify(|_, w| w.moder5().output());
-    gpioa.otyper.modify(|_, w| w.ot5().push_pull());
-    gpioa.ospeedr.modify(|_, w| w.ospeedr5().low_speed());
+    // Configure PC13 as push-pull output
+    let gpioc = dp.GPIOC;
+    gpioc.moder.modify(|_, w| w.moder13().output()); // Set PC13 as output
+    gpioc.otyper.modify(|_, w| w.ot13().push_pull()); // Set PC13 as push-pull
+    gpioc.ospeedr.modify(|_, w| w.ospeedr13().low_speed()); // Set low speed
 
     loop {
-        // Toggle the LED
-        gpioa.odr.modify(|r, w| w.odr5().bit(!r.odr5().bit()));
+        // Turn the LED on (set PC13 high)
+        gpioc.odr.modify(|_, w| w.odr13().set_bit());
+        cortex_m::asm::delay(8_000_000); // Delay
 
-        // Delay (basic busy-loop delay)
-        for _ in 0..1_000_000 {
-            cortex_m::asm::nop();
-        }
+        // Turn the LED off (set PC13 low)
+        gpioc.odr.modify(|_, w| w.odr13().clear_bit());
+        cortex_m::asm::delay(8_000_000); // Delay
     }
 }
